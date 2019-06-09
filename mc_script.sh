7fc164c13809fbc983a4b932311524682573d841
482F <482F.git.mail@482f.net>
2019-06-10 00:00:00 +0900

    インデントを調整

--- mc_script.sh
+++ mc_script.sh
@@ -69,37 +69,37 @@ BK_GEN="3"
 cd $MC_PATH
  
 if [ ! -d $BK_DIR ]; then
-  mkdir $BK_DIR
+    mkdir $BK_DIR
 fi
  
 ME=`whoami`
  
 if [ $ME != $USERNAME ]; then
-  echo "Please run the $USERNAME user."
-  exit
+    echo "Please run the $USERNAME user."
+    exit
 fi
  
 start() {
-  if pgrep -u $USERNAME -f "${SCNAME} java" > /dev/null; then
-    echo "$SERVICE is already running!"
-    exit
-  fi
-  echo "Starting $SERVICE..."
-  screen -AmdS $SCNAME java -Xmx$XMX -Xms$XMS -jar $SERVICE nogui
+    if pgrep -u $USERNAME -f "${SCNAME} java" > /dev/null; then
+        echo "$SERVICE is already running!"
+            exit
+    fi
+    echo "Starting $SERVICE..."
+    screen -AmdS $SCNAME java -Xmx$XMX -Xms$XMS -jar $SERVICE nogui
 }
  
 stop() {
-  if pgrep -u $USERNAME -f "${SCNAME} java" > /dev/null; then
-    echo "Stopping $SERVICE"
-    screen -p 0 -S $SCNAME -X eval 'stuff "say SERVER SHUTTING DOWN IN 3 SECONDS. Saving map..."\015'
-    screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
-    sleep 3
-    screen -p 0 -S $SCNAME -X eval 'stuff "stop"\015'
-    sleep 10
-    echo "Stopped minecraftserver"
-  else
-    echo "$SERVICE is not running!"
-  fi
+    if pgrep -u $USERNAME -f "${SCNAME} java" > /dev/null; then
+        echo "Stopping $SERVICE"
+        screen -p 0 -S $SCNAME -X eval 'stuff "say SERVER SHUTTING DOWN IN 3 SECONDS. Saving map..."\015'
+        screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
+        sleep 3
+        screen -p 0 -S $SCNAME -X eval 'stuff "stop"\015'
+        sleep 10
+        echo "Stopped minecraftserver"
+    else
+        echo "$SERVICE is not running!"
+    fi
 }
 
 reload() {
@@ -108,51 +108,51 @@ reload() {
 }
  
 h_backup() {
-  if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
-    echo "Backup start minecraft data..."
-    screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
-    sleep 10
-    screen -p 0 -S $SCNAME -X eval 'stuff "save-off"\015'
-    tar cfv $HOUR_BK_NAME $BK_FILE
-    sleep 10
-    screen -p 0 -S $SCNAME -X eval 'stuff "save-on"\015'
-    echo "minecraft_server backup compleate!"
-    gzip -f $HOUR_BK_NAME
-    find $BK_DIR -name "mc_backup_hourly_*.tar.gz" -type f -mtime +$BK_GEN -exec rm {} \;
-  else
-    echo "$SERVICE was not runnning."
-  fi
+    if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
+        echo "Backup start minecraft data..."
+        screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
+        sleep 10
+        screen -p 0 -S $SCNAME -X eval 'stuff "save-off"\015'
+        tar cfv $HOUR_BK_NAME $BK_FILE
+        sleep 10
+        screen -p 0 -S $SCNAME -X eval 'stuff "save-on"\015'
+        echo "minecraft_server backup compleate!"
+        gzip -f $HOUR_BK_NAME
+        find $BK_DIR -name "mc_backup_hourly_*.tar.gz" -type f -mtime +$BK_GEN -exec rm {} \;
+    else
+        echo "$SERVICE was not runnning."
+    fi
 }
  
 f_backup() {
-  if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
-    echo "Full backup start minecraft data..."
-    screen -p 0 -S $SCNAME -X eval 'stuff "say サーバーの再起動が約 10 秒後に行われます。"\015'
-    screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
-    sleep 10
-    screen -p 0 -S $SCNAME -X eval 'stuff "stop"\015'
-    echo "Stopped minecraft_server"
-    echo "Full Backup start ..."
-    screen -ls
-    tar cfvz $FULL_BK_NAME $MC_PATH
-    sleep 10
-    echo "Full Backup compleate!"
-    find $BK_DIR -name "mc_backup_full*.tar.gz" -type f -mtime +$BK_GEN -exec rm {} \;
-    echo "Starting $SERVICE..."
-    screen -AmdS $SCNAME java -Xmx$XMX -Xms$XMS -jar $SERVICE nogui
-  else
-    echo "$SERVICE was not runnning."
-  fi
+    if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
+        echo "Full backup start minecraft data..."
+        screen -p 0 -S $SCNAME -X eval 'stuff "say サーバーの再起動が約 10 秒後に行われます。"\015'
+        screen -p 0 -S $SCNAME -X eval 'stuff "save-all"\015'
+        sleep 10
+        screen -p 0 -S $SCNAME -X eval 'stuff "stop"\015'
+        echo "Stopped minecraft_server"
+        echo "Full Backup start ..."
+        screen -ls
+        tar cfvz $FULL_BK_NAME $MC_PATH
+        sleep 10
+        echo "Full Backup compleate!"
+        find $BK_DIR -name "mc_backup_full*.tar.gz" -type f -mtime +$BK_GEN -exec rm {} \;
+        echo "Starting $SERVICE..."
+        screen -AmdS $SCNAME java -Xmx$XMX -Xms$XMS -jar $SERVICE nogui
+    else
+        echo "$SERVICE was not runnning."
+    fi
 }
  
 status() {
-  if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
-    echo "$SERVICE is already running!"
-    exit
-  else
-    echo "$SERVICE is not running!"
-    exit
-  fi
+    if pgrep -u $USERNAME -f $SERVICE > /dev/null; then
+        echo "$SERVICE is already running!"
+        exit
+    else
+        echo "$SERVICE is not running!"
+        exit
+    fi
 }
 
 get_motd() {
@@ -167,30 +167,30 @@ set_motd() {
 
  
 case "$1" in
-  start)
-    start
-    ;;
-  stop)
-    stop
-    ;;
-  reload)
-    reload
-    ;;
-  h_backup)
-    h_backup
-    ;;
-  f_backup)
-    f_backup
-    ;;
-  status)
-    status
-    ;;
-  get_motd)
-    get_motd
-    ;;
-  set_motd)
-    set_motd "$2"
-    ;;
-  *)
-    echo  $"Usage: $0 {start|stop|reload|h_backup|f_backup|status|(get|set)_motd}"
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    reload)
+        reload
+        ;;
+    h_backup)
+        h_backup
+        ;;
+    f_backup)
+        f_backup
+        ;;
+    status)
+        status
+        ;;
+    get_motd)
+        get_motd
+        ;;
+    set_motd)
+        set_motd "$2"
+        ;;
+    *)
+        echo  $"Usage: $0 {start|stop|reload|h_backup|f_backup|status|(get|set)_motd}"
 esac
