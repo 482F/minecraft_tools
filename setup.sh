74780317af6ebe8417d6b4f7b471b1a401bef425
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-06-12 11:49:52 +0900

    複数のサーバーを同時に稼働させられるようになったので、change_target.sh は廃止

--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-MAIN_DIR=$(eval 'echo ~/minecraft')
-MINECRAFT_DIR=$(eval 'echo ~/minecraft_directory')
+MINECRAFT_DIR=$(eval 'echo ~/minecraft')
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-
-CHANGE_TARGET_SH="change_target.sh"
 
 if [ ! -d "${MINECRAFT_DIR}" ]; then
     mkdir "${MINECRAFT_DIR}"
-fi
-
-if [ ! -f "${MINECRAFT_DIR}/${CHANGE_TARGET_SH}" ]; then
-    ln -s "${SCRIPT_DIR}/${CHANGE_TARGET_SH}" "${MINECRAFT_DIR}/${CHANGE_TARGET_SH}"
 fi
