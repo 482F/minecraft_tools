043ab6c3ae1ac1f2bcf1a33765958ea0534e0098
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-06-12 12:04:18 +0900

    mc_script.sh を配布するように

--- setup.sh
+++ setup.sh
@@ -6,3 +6,14 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 if [ ! -d "${MINECRAFT_DIR}" ]; then
     mkdir "${MINECRAFT_DIR}"
 fi
+
+SERVERS_DIR="${MINECRAFT_DIR}/servers"
+
+SERVERS="$(ls -1 "${SERVERS_DIR}")"
+
+echo "${SERVERS}" | while read SERVER; do
+    SERVER="${SERVERS_DIR}/${SERVER}"
+    if [ ! -f "${SCRIPT_DIR}/mc_script.sh" ]; then
+        ln -s "${SCRIPT_DIR}/mc_script.sh" "${SERVER}/mc_script.sh"
+    fi
+done
